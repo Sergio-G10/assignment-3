45a2e0793b0c7001bebbc71abd9eac0c59da5b3a
sergio-1099 <sergioguerra6188@gmail.com>
2025-09-26 12:32:11 -0400

    Add running total

--- script.js
+++ script.js
@@ -5,11 +5,31 @@ let cards = document.querySelectorAll('.card');
 let main = document.querySelector('main');
 let summary = document.createElement('div');
 summary.id = 'summary';
+
 let summaryHeading = document.createElement('h3');
 let summaryHeadingText = document.createTextNode('Favorites Summary');
+
 summaryHeading.appendChild(summaryHeadingText);
 summary.appendChild(summaryHeading);
 main.appendChild(summary);
+
+// Add Running Total below Summary
+let runningTotal = document.createElement('div');
+runningTotal.id = 'total';
+
+let runningTotalHeading = document.createElement('h4');
+let totalHeadingText = document.createTextNode('Total');
+runningTotalHeading.appendChild(totalHeadingText);
+
+let totalValue = document.createElement('p');
+totalValue.id = 'value';
+let totalValueText = document.createTextNode('$0');
+totalValue.appendChild(totalValueText);
+
+runningTotal.appendChild(runningTotalHeading);
+runningTotal.appendChild(totalValue);
+main.appendChild(runningTotal);
+
 
 // Loop through all cards and add price tag and button
 for (let i = 0; i < cards.length; i++) {
@@ -48,14 +68,23 @@ function addToFavorites(cardID) {
   let currentButton = card.querySelector('button');
   currentButton.textContent = 'Remove Favorite';
 
+  // Create new favorite div and give unique ID based on card number
   var newFavorite = document.createElement('div');
   newFavorite.id = cardID;
   var favoriteName = card.querySelector('h5').textContent;
-  var price = card.querySelector('div').textContent;
-  var favoriteText = document.createTextNode(favoriteName + ': ' + price);
+
+  var price = document.createElement('span');
+  var priceText = card.querySelector('div').textContent;
+  var priceTextNode = document.createTextNode(priceText);
+  price.appendChild(priceTextNode);
+
+  var favoriteText = document.createTextNode(favoriteName + ': ');
   
   newFavorite.appendChild(favoriteText);
+  newFavorite.appendChild(price);
   summary.appendChild(newFavorite);
+
+  calculateNewTotal();
 }
 
 function removeFavorite(cardID) {
@@ -65,6 +94,23 @@ function removeFavorite(cardID) {
   let currentButton = card.querySelector('button');
   currentButton.textContent = 'Add to Favorites';
 
+  // Remove favorite div based on card number
   var favorite = document.getElementById(cardID);
   favorite.remove();
+
+  calculateNewTotal();
+}
+
+function calculateNewTotal() {
+  let total = 0;
+
+  let favorites = summary.querySelectorAll('div');
+
+  for (let i = 0; i < favorites.length; i++) {
+    var price = favorites[i].querySelector('span').textContent.substring(1);
+
+    total += parseInt(price);
+  }
+
+  totalValue.textContent = '$' + total;
 }
